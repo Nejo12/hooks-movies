4019e806b973906ad88db3c6434d9d48d21fd0ed
Gabriel <olaniyi_g@hotmail.com>
2020-01-09 13:29:54 +0100

    Structure of Movie pages created.

---
+++ src/components/navigation/navigation.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Router } from "@reach/router";
+
+const Navigation = () => {
+  return <div>Navigation ....</div>;
+};
+
+export default Navigation;
--- src/pages/movies/movie.component.jsx
+++ src/pages/movies/movie.component.jsx
@@ -1,5 +1,24 @@
 import React from "react";
 
-const Movie = ({ movieId }) => <div>Movie: {movieId}</div>;
+import Navigation from "../../components/navigation/navigation.component";
+import MovieInfo from "../../components/movie-info/movie-info.component";
+import MovieInfoBar from "../../components/movie-info-bar/movie-info-bar.component";
+import Actors from "../../components/actors/actors.component";
+import Grid from "../../components/grid/grid.component";
+import Spinner from "../../components/spinner/spinner.component";
+
+const Movie = ({ movieId }) => {
+  return (
+    <div>
+      <Navigation />
+      <MovieInfo />
+      <MovieInfoBar />
+      <Grid>
+        <Actors />
+      </Grid>
+      <Spinner />
+    </div>
+  );
+};
 
 export default Movie;
